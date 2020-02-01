99c7cc361623d7cf575d08e470264c488026be9f
root <eugenekurilov@gmail.com>
2020-02-01 19:21:07 +0300

    added file for doctrine config

---
+++ bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+//require_once __DIR__ . "/vendor/autoload.php";
+require(__DIR__ . '/config.php');
+$isDevMode = false;
+// Настройки будут браться из аннотаций, на мой взгляд, это удобнее
+// Заметьте, что здесь я передаю путь до каталога, который будет содержать в себе классы сущностей, проецируемые на БД
+$config = Setup::createAnnotationMetadataConfiguration(
+    [
+        __DIR__."/src/entities/"
+    ],
+    $isDevMode
+);
+
+//Здесь вам надо вытащить настройки подключения к БД для вашего проекта
+$dbParams = [
+    'driver'   => $parameters['driver'],
+    'user'     => $parameters['user'],
+    'password' => $parameters['password'],
+    'dbname'   => $parameters['dbname'],
+];
+/** @var EntityManager $em */
+$em = EntityManager::create($dbParams, $config);
